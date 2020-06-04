f6d76fbd0311ddbf108b72524cf91fff14dc2b15
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-06-04 00:09:47 -0500

    Created initial !rat help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,35 +2,29 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "help",
-    description: "Shows all of the commands available to the bot",
+    description: "Shows all of the commands available",
     execute(msg, args) {
         const exampleEmbed = new Discord.MessageEmbed()
             .setColor("#66757f")
-            .setTitle("!rat Commands")
+            .setTitle("!rat commands")
             // .setURL("")
-            .setDescription("Some description here")
-            .attachFiles(["../rat_logo.png"])
-            .setThumbnail("attachment://rat_logo.png")
+            // .setDescription("All commands start with !rat")
+            .setThumbnail(
+                "https://raw.githubusercontent.com/RileyAbr/rat-facts-Discord-Bot/master/rat_logo.png"
+            )
             .addFields(
-                { name: "Regular field title", value: "Some value here" },
-                { name: "\u200B", value: "\u200B" },
                 {
-                    name: "Inline field title",
-                    value: "Some value here",
-                    inline: true,
+                    name: "!rat fact (!rat)",
+                    value: "Gives a random fact about rats",
                 },
                 {
-                    name: "Inline field title",
-                    value: "Some value here",
-                    inline: true,
+                    name: "!rat help",
+                    value: "Shows all of the commands available",
                 }
             )
-            .addField("Inline field title", "Some value here", true)
-            .setImage("https://i.imgur.com/wSTFkRM.png")
-            .setTimestamp()
             .setFooter(
-                "Some footer text here",
-                "https://i.imgur.com/wSTFkRM.png"
+                "rat bot created by Riley Abrahamson",
+                "https://avatars3.githubusercontent.com/u/32375220?s=460&u=a375968682555bb66226556bf7ee33d6d35b7961&v=4"
             );
 
         msg.channel.send(exampleEmbed);
