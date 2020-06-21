96ff0c795922398a15401d03e35b572798ed4b84
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-06-21 16:42:20 -0500

    !rat author command completed

--- src/commands/main/author.js
+++ src/commands/main/author.js
@@ -7,17 +7,23 @@ module.exports = {
         const authorEmbed = new Discord.MessageEmbed()
             .setColor("#66757f")
             .setTitle("rat facts Discord Bot")
+            .setURL("https://github.com/RileyAbr/rat-facts-Discord-Bot")
+            .setDescription("Created by Riley Abrahamson.")
             .setThumbnail(
                 "https://raw.githubusercontent.com/RileyAbr/rat-facts-Discord-Bot/master/rat_logo.png"
             )
             .addFields({
-                name: "!rat fact (!rat)",
-                value: "Gives a random fact about rats",
+                name: "Riley's Website",
+                value: "www.rileyabrahamson.com",
             })
-            .setFooter(
-                "rat facts created by Riley Abrahamson",
-                "https://avatars3.githubusercontent.com/u/32375220?s=460&u=a375968682555bb66226556bf7ee33d6d35b7961&v=4"
-            );
+            .addFields({
+                name: "Twitter",
+                value: "@RileyAbrahamson",
+            })
+            .addFields({
+                name: "GitHub",
+                value: "RileyAbr",
+            });
 
         msg.channel.send(authorEmbed);
     },
