eb28396caa386016a022a99714cf9d4969f6b6e2
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-25 16:41:02 -0700

    Add joke command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,5 +1,6 @@
 import { Command } from './Command'
 import { fact } from './commands/fact'
+import { joke } from './commands/joke'
 import { pic } from './commands/pic'
 
-export const Commands: Command[] = [fact, pic]
+export const Commands: Command[] = [fact, pic, joke]
