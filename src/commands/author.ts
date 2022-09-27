0825796457aafbaa003b0ba45de45b741ba80b47
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-27 16:47:42 -0700

    Add shared variables to author command

--- src/commands/author.ts
+++ src/commands/author.ts
@@ -5,7 +5,7 @@ import {
   EmbedBuilder,
 } from 'discord.js'
 import { Command } from '../Command'
-import { embedColor } from '../data/embedVariables'
+import { embedColor, embedThumbnail } from '../data/embedVariables'
 
 export const author: Command = {
   name: 'author',
@@ -17,9 +17,7 @@ export const author: Command = {
       .setTitle('rat facts Discord Bot')
       .setURL('https://github.com/RileyAbr/rat-facts-Discord-Bot')
       .setDescription('Created by Riley Abrahamson.')
-      .setThumbnail(
-        'https://raw.githubusercontent.com/RileyAbr/rat-facts-Discord-Bot/master/rat_logo.png',
-      )
+      .setThumbnail(embedThumbnail)
       .addFields({
         name: "Riley's Website",
         value: 'www.rileyabrahamson.com',
