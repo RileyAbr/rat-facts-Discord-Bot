e53d6550d82cdc65f2efee6cefff7cf34c4f90fb
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-26 20:05:11 -0700

    Add 8Ball command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,6 +1,7 @@
 import { Command } from './Command'
+import { eightBall } from './commands/8ball'
 import { fact } from './commands/fact'
 import { joke } from './commands/joke'
 import { pic } from './commands/pic'
 
-export const Commands: Command[] = [fact, pic, joke]
+export const Commands: Command[] = [fact, pic, joke, eightBall]
