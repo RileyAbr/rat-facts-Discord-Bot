6cba345a1b156bc8d0166632e162b6068c5e79f0
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-27 16:47:25 -0700

    Add help command

---
+++ src/commands/help.ts
@@ -0,0 +1,87 @@
+import {
+  ApplicationCommandType,
+  Client,
+  CommandInteraction,
+  EmbedBuilder,
+} from 'discord.js'
+import { Command } from '../Command'
+import {
+  embedAttributionFooter,
+  embedColor,
+  embedThumbnail,
+} from '../data/embedVariables'
+import { eightBall } from './8ball'
+import { author } from './author'
+import { donate } from './donate'
+import { fact } from './fact'
+import { joke } from './joke'
+import { pic } from './pic'
+
+const donateLink = 'https://ko-fi.com/rileyabrahamson'
+
+const helpName = 'help'
+const helpDescription = 'Lists all of the commands available to rat facts'
+
+export const help: Command = {
+  name: helpName,
+  description: helpDescription,
+  type: ApplicationCommandType.ChatInput,
+  run: async (client: Client, interaction: CommandInteraction) => {
+    const helpEmbed = new EmbedBuilder()
+      .setColor(embedColor)
+      .setTitle('`rat facts` / commands')
+      .setThumbnail(embedThumbnail)
+      .addFields(
+        {
+          name: '/' + fact.name,
+          value: fact.description,
+        },
+        {
+          name: '/' + pic.name,
+          value: pic.description,
+        },
+        {
+          name: '/' + joke.name,
+          value: joke.description,
+        },
+        {
+          name: '/' + eightBall.name + ' [question]',
+          value: eightBall.description,
+        },
+        // {
+        //   name: quiz.name,
+        //   value: quiz.description,
+        // },
+        // {
+        //   name: role.name,
+        //   value: role.description,
+        // },
+        // {
+        //   name: lore.name,
+        //   value: lore.description,
+        // },
+        {
+          name: '/' + author.name,
+          value: author.description,
+        },
+        {
+          name: '/' + helpName,
+          value: helpDescription,
+        },
+        {
+          name: '/' + donate.name,
+          value: donate.description,
+        },
+        // {
+        //   name: misc.name,
+        //   value: misc.description,
+        // },
+      )
+      .setFooter(embedAttributionFooter)
+
+    await interaction
+      .followUp({ embeds: [helpEmbed] })
+      .then()
+      .catch(error => console.error(`ERROR: ${error}`))
+  },
+}
