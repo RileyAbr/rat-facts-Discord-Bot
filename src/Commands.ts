6d9424454e9e9f65e6aea95bc7f69de0cd72dde7
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-27 16:47:33 -0700

    Update Commands.ts

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,8 +1,18 @@
 import { Command } from './Command'
 import { eightBall } from './commands/8ball'
 import { author } from './commands/author'
+import { donate } from './commands/donate'
 import { fact } from './commands/fact'
+import { help } from './commands/help'
 import { joke } from './commands/joke'
 import { pic } from './commands/pic'
 
-export const Commands: Command[] = [fact, pic, joke, eightBall, author]
+export const Commands: Command[] = [
+  fact,
+  pic,
+  joke,
+  eightBall,
+  author,
+  help,
+  donate,
+]
