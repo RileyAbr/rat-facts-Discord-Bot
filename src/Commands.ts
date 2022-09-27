e3378b0ada043d27c0530936aa41ef8de87f0ac9
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-09-26 20:08:08 -0700

    Add author command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,7 +1,8 @@
 import { Command } from './Command'
 import { eightBall } from './commands/8ball'
+import { author } from './commands/author'
 import { fact } from './commands/fact'
 import { joke } from './commands/joke'
 import { pic } from './commands/pic'
 
-export const Commands: Command[] = [fact, pic, joke, eightBall]
+export const Commands: Command[] = [fact, pic, joke, eightBall, author]
---
+++ src/commands/author.ts
@@ -0,0 +1,40 @@
+import {
+  ApplicationCommandType,
+  Client,
+  CommandInteraction,
+  EmbedBuilder,
+} from 'discord.js'
+import { Command } from '../Command'
+
+export const author: Command = {
+  name: 'author',
+  description: 'Provides the author and social links for the bot',
+  type: ApplicationCommandType.ChatInput,
+  run: async (client: Client, interaction: CommandInteraction) => {
+    const authorEmbed = new EmbedBuilder()
+      .setColor('#66757f')
+      .setTitle('rat facts Discord Bot')
+      .setURL('https://github.com/RileyAbr/rat-facts-Discord-Bot')
+      .setDescription('Created by Riley Abrahamson.')
+      .setThumbnail(
+        'https://raw.githubusercontent.com/RileyAbr/rat-facts-Discord-Bot/master/rat_logo.png',
+      )
+      .addFields({
+        name: "Riley's Website",
+        value: 'www.rileyabrahamson.com',
+      })
+      .addFields({
+        name: 'Twitter',
+        value: '@RileyAbrahamson',
+      })
+      .addFields({
+        name: 'GitHub',
+        value: 'RileyAbr',
+      })
+
+    await interaction
+      .followUp({ embeds: [authorEmbed] })
+      .then()
+      .catch(error => console.error(`ERROR: ${error}`))
+  },
+}
