8e144d8a365b31cf9928284b0b27a99d81fc9712
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2022-10-02 13:04:48 -0700

    Remove previous rat facts code

--- src/commands/misc/anthem.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "anthem",
-    commandUsage: "!rat anthem",
-    description: "Links the anthem of rats",
-    execute(msg, args) {
-        msg.channel.send("https://www.youtube.com/watch?v=OXQwx1EolD8");
-    },
-};
--- src/scripts/mostRecentRelease.js
+++
@@ -1,5 +0,0 @@
-const version = require("../../package.json").version;
-
-const repoUrl = "https://github.com/RileyAbr/rat-facts-Discord-Bot/";
-
-exports.releaseLink = `${repoUrl}releases/tag/v${version}`;
